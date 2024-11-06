23aa23ac161fa1f662eefb4c3edbca66654fd58b
Elliott <elynch90@gmail.com>
2024-11-05 21:51:28 -0800

    Update main.py

--- src/models/logistic_reg/main.py
+++ src/models/logistic_reg/main.py
@@ -6,26 +6,24 @@ EPSILON = 1e-9
 LEARNING_RATE = 0.1
 
 
-def sigmoid(z):
+def sigmoid(z) -> float:
+    """"""
     return 1 / (1 + exp(-z))
 
 
-def forward(x=None, theta=None, bias=0):
+def forward(x=None, theta=None, bias=0) -> float:
+    """"""
     return dot(x, theta) + bias
 
 
-def loss_func(y, y_hat):
-    return -y * log(y_hat + EPSILON) - (1 - y) * log(1 - y_hat + EPSILON)
+def loss_func(y, y_hat, epsilon=EPSILON) -> float:
+    """"""
+    return -y * log(y_hat + epsilon) - (1 - y) * log(1 - y_hat + epsilon)
 
 
-def main() -> int:
-    # Data
-    X = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    y = [0, 1, 1, 1]
-    X_test = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    y_test = [0, 1, 1, 1]
-    # initialize weights
-    theta = [0.1, 0.2]
+def stochastic_gradient_descent(
+        X, Y, theta, epochs=EPOCHS, learning_rate=LEARNING_RATE):
+    """"""
     for epoch in range(EPOCHS):
         for i, x in enumerate(X):
             # forward pass
@@ -33,9 +31,9 @@ def main() -> int:
             # activation function
             y_hat = sigmoid(logit)
             # calculate loss
-            loss = loss_func(y[i], y_hat)
+            loss = loss_func(Y[i], y_hat)
             # calculate error (derivative of the loss function)
-            error = y_hat - y[i]
+            error = y_hat - Y[i]
             print(f"Epoch: {epoch}, Loss: {loss}, Error: {error}")
             # backpropagation: update weights
             for j in range(len(theta)):
@@ -43,7 +41,26 @@ def main() -> int:
                 # the gradients tell us which direction will increase the loss
                 # so we subtract the gradient to decrease the loss instead
                 # like opposite George
-                theta[j] -= LEARNING_RATE * error * x[j]
+                theta[j] -= learning_rate * error * x[j]
+    return theta
+
+
+def predict(x, theta):
+    """"""
+    logit = forward(x, theta)
+    return ceil(sigmoid(logit))
+
+
+def main() -> int:
+    # Data
+    X = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    Y = [0, 1, 1, 1]
+    X_test = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    y_test = [0, 1, 1, 1]
+    # initialize weights
+    theta = [0.1, 0.2]
+    # Train model aka optimize weights
+    theta = stochastic_gradient_descent(X, Y, theta)
     # Test
     for i, x in enumerate(X_test):
         logit = forward(x, theta)
